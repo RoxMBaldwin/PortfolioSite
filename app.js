5bae0aa04211fad8876b02f6288f3b1069d097fe
Roxanne Baldwin <rox.m.baldwin@gmail.com>
2018-02-15 14:56:45 -0700

    🍦  updated repository with current code

---
+++ app.js
@@ -0,0 +1,23 @@
+// anime({
+//   targets: 'div',
+//   translateX: [
+//     { value: 100, duration: 1200 },
+//     { value: 0, duration: 800 }
+//   ],
+//   rotate: '1turn',
+//   backgroundColor: '#F7D9FF',
+//   duration: 4000,
+//   loop: true
+// });
+
+var lineDrawing = anime({
+  targets: '#lineDrawing .lines path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 1500,
+  delay: function(el, i) { return i * 250 },
+  direction: 'alternate',
+  loop: true
+});
+
+console.log('working');

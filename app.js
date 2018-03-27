6922212f484f01b9c92806e8fdcc964aaabebe8d
Roxanne Baldwin <rox.m.baldwin@gmail.com>
2018-03-27 17:09:36 -0600

    📱  added mobile responsiveness // stacking nav links

--- app.js
+++ app.js
@@ -10,6 +10,34 @@
 //   loop: true
 // });
 
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('innit')
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+
 var lineDrawing = anime({
   targets: '#lineDrawing .lines path',
   strokeDashoffset: [anime.setDashoffset, 0],
